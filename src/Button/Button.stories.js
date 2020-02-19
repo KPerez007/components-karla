fb4eee66ca8de78e70b42de4d9eb09b541c8970c
kperez <karla.perez.o@dga.gob.ni>
2020-02-19 15:31:01 -0600

    agregado de otro proyecto

---
+++ src/Button/Button.stories.js
@@ -0,0 +1,25 @@
+// @flow
+import React from "react"
+import { action } from '@storybook/addon-actions';
+
+import Button from "."
+
+export default {
+  title: "Button",
+  component: Button,
+  parameters: {
+    info: {
+      inline: true
+    }
+  }
+}
+
+export const defaultButton = () => <Button onClick={action("button-click")} >Click aquí</Button>
+
+export const mediumButton = () => <Button size="medium" onClick={action("button-click")}>Click aquí</Button>
+
+export const underlineButton = () => (
+  <Button size="medium" underline onClick={action("button-click")}>
+    Click aquí
+  </Button>
+)
