6a5add140303a96e728fa293a2c5eefce969f907
kperez <karla.perez.o@dga.gob.ni>
2020-02-22 09:33:20 -0600

    se agregaron componentes

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// @flow
+
+//import { Button } from 'components-karla'
+//import Button from "./Button";
+//import theme from "./theme";
+
+//export { theme, Button };
+
+export { default as theme } from "./theme";
+export { default as Button } from "./Button";
